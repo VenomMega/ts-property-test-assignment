e94dba35a8c7a534c2a04157e1c747e612ea7765
Alibek <venommega2001@gmail.com>
2023-10-06 16:11:32 +0600

    init commit

---
+++ kafka-receive-service/src/main/java/kz/alibek/kafkareceiveservice/KafkaReceiveServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.alibek.kafkareceiveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReceiveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaReceiveServiceApplication.class, args);
+    }
+
+}
